5a97d9cc308b1c383e427dba7c54b8f78b481653
Huntress77 <Hjones12074@gmail.com>
2024-07-22 08:53:46 +1200

    Bugfix

--- Scripts/Gameplay/MapGrounds/AddGround5.gd
+++ Scripts/Gameplay/MapGrounds/AddGround5.gd
@@ -1,6 +1,6 @@
 extends Node
 @export var GroundRight:PackedScene
-@onready var Besprite = preload("res://Sprites/BehindGround.png")
+@onready var Besprite = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
